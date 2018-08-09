7a5d5922a4bb3a9f9b61ab8f7edbd3439b120747
Luis Antonio García Perrusquía <lgarciaperrusquia@gmail.com>
2018-08-09 16:23:03 -0500

    timeLinePrev

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,12 +2,44 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-  }
+	public chismes = [
+		{
+			hrs: 1,
+			mtr: 100,
+			texto: 'Ximena volvio a salir con Alexis y luego dice que no le gusta que por que está más chico que ella... si le lleva 5 años,que asalta cunas',
+			gusta: 300
+		},
+		{
+			hrs: 10,
+			mtr: 300,
+			texto: 'Lupita la hija de la tienda esta embarzada de arturo el de la carniceria.',
+			gusta: 300
+		},
+		{
+			hrs: 10,
+			mtr: 200,
+			texto: 'Ayer en el cine cerca de insurgentes encontre a Arturo con una chica que no era su novia, la llevaba de la mano.',
+			gusta: 300
+		},
+		{
+			hrs: 6,
+			mtr: 500,
+			texto: 'La mama de José esta teniendo sexo con el papá de Jessica.',
+			gusta: 300
+		},
+		{
+			hrs: 5,
+			mtr: 100,
+			texto: 'Ximena volvio a salir con Alexis y luego dice que no le gusta que por que está más chico que ella... si le lleva 5 años,que asalta cunas',
+			gusta: 300
+		},
+	];
+
+	constructor(public navCtrl: NavController) { }
 
 }
